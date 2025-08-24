26ac0ef26ae4636121a08f5e0431c3a228144e2c
Kele1996 <angelamooketsi5@gmail.com>
2025-08-24 15:50:26 +0200

    added axios to project

--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ let cityElement = document.querySelector("#city");
 cityElement.innerHTML = searchInput.value;
 }
 
-
-
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
